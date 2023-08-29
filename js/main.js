6e873497d0e61f3d46b34fb3241c3640434ac259
Reubi0910 <esteban.morillo@divtec.ch>
2023-08-29 16:19:26 +0200

    addition of a list addition of "articles" in the list based on the text in the input

--- js/main.js
+++ js/main.js
@@ -7,14 +7,20 @@
 
 "use strict";
 
-// Récupérer tous les paragraphes du document
+// Récupérer tous les paragraphes du document, le bouton ajouter
 
 let tabPara = document.querySelectorAll("p");
-
-// Récupérer le bouton ajouter
-
-let button = document.querySelector("button.ajouter");
+let btAjouter = document.querySelector("button.ajouter");
+let txtArticle = document.querySelector("#article");
+let ulArticles = document.querySelector("ul.articles");
 
 // Affiche le contenu des variables dans la console
 
-console.log(tabPara,button);
+console.log(tabPara,btAjouter,txtArticle);
+
+// Écoute le click sur le bouton ajouter
+
+btAjouter.addEventListener("click", () => {
+    ulArticles.innerHTML += "<li>" + txtArticle.value + "</li>";
+    txtArticle.value = "";
+});
